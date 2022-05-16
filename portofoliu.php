9e5fbdcc77d2fb76b99096aa4fa972929e73e441
Gabi <gabrielgabi101@gmail.com>
2022-05-16 15:49:01 +0300

    ->Portofoliu.php -fancybox

---
+++ portofoliu.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php 
+		include 'templates/head.html'
+	?>
+</head>
+<body>
+	<?php 
+		include 'templates/navbar.html'
+	?>
+<div class="container">
+	<div class="container-fluid">
+		<div class="row mt-4">
+			<div class="item col-sm-6 col-md-4 mb" style="padding-bottom: 6px;">
+				<a href="images/image1.jpg" class="fancybox" data-fancybox="gallery1">
+					<img src="images/image1.jpg" width="100%" height="100%">
+				</a>
+			</div>
+			<div class="item col-sm-6 col-md-4 mb" style="padding-bottom: 6px;">
+				<a href="images/image2.jpg" class="fancybox" data-fancybox="gallery1">
+					<img src="images/image2.jpg" width="100%" height="100%">
+				</a>
+			</div>
+			<div class="item col-sm-6 col-md-4 mb" style="padding-bottom: 6px;">
+				<a href="images/image3.jpg" class="fancybox" data-fancybox="gallery1">
+					<img src="images/image3.jpg" width="100%" height="100%">
+				</a>
+			</div>
+			<div class="item col-sm-6 col-md-4 mb" style="padding-bottom: 6px;">
+				<a href="images/image4.jpg" class="fancybox" data-fancybox="gallery1">
+					<img src="images/image4.jpg" width="100%" height="100%">
+				</a>
+			</div>
+			<div class="item col-sm-6 col-md-4 mb" style="padding-bottom: 6px;">
+				<a href="images/image5.jpg" class="fancybox" data-fancybox="gallery1">
+					<img src="images/image5.jpg" width="100%" height="100%">
+				</a>
+			</div>
+			<div class="item col-sm-6 col-md-4 mb" style="padding-bottom: 6px;">
+				<a href="images/image6.jpg" class="fancybox" data-fancybox="gallery1">
+					<img src="images/image6.jpg" width="100%" height="100%">
+				</a>
+			</div>
+
+
+		</div>
+		
+	</div>
+	</div>
+</body>
+</html>
